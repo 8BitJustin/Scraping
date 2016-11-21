66a313e884657a0d4bea608ce1802e6dcda14eed
Justin Olson <jro34@live.com>
2016-11-20 20:12:55 -0700

    Created prettify test script. Shows how to display the HTML of a desired site

---
+++ Pretify Test.py
@@ -0,0 +1,23 @@
+import urllib
+import urllib.request
+from bs4 import BeautifulSoup
+
+"""
+# This is when you set the variables, and print. It creates the page in HTML format
+
+thepage = urllib.request.urlopen('http://www.arizona.edu/')
+soupdata = BeautifulSoup(thepage, 'html.parser')
+
+print(soupdata.prettify())
+"""
+
+# This is if you make it a function and run it
+
+def make_soup(url):
+    thepage = urllib.request.urlopen(url)
+    soupdata = BeautifulSoup(thepage, "html.parser")
+    return soupdata
+
+soup = make_soup("http://www.arizona.edu/")
+
+print(soup.prettify())
